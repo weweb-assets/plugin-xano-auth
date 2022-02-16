236f639d1141aa3fceaf3aa13ba6a34f007f35fc
Dams-d <damien.duro8@gmail.com>
2022-02-16 17:16:07 +0100

    remove design system id from config

--- ww-config.js
+++ ww-config.js
@@ -22,7 +22,7 @@ export default {
                 },
             },
         ],
-        designSystemId: 'ec2eebfe-499b-43c4-b260-80ee5a4d9504',
+        // designSystemId: 'ec2eebfe-499b-43c4-b260-80ee5a4d9504',
     },
     variables: [
         { name: 'user', value: 'user', type: 'object', defaultValue: null },
