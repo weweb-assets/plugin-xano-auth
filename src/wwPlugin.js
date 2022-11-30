da4464a6f38b48572dde3371fa203ab51751d3bd
Alexis JC <alexisjc.dev@gmail.com>
2022-11-30 11:10:36 +0100

    fix: typo

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -105,11 +105,10 @@ export default {
             throw err;
         }
     },
-    async continueLoginProvider(pendingLogin) {
+    async continueLoginProvider({ provider, type, redirectUrl }) {
         try {
-            const { name, api } = pendingLogin.provider;
-            const endpoint = name === 'twitter-oauth' ? 'access_token' : pendingLogin.type;
-            const result = await axios.get(`${api}/oauth/${name.split('-')[0]}/${endpoint}`, {
+            const endpoint = provider.name === 'twitter-oauth' ? 'access_token' : type;
+            const result = await axios.get(`${provider.api}/oauth/${provider.name.split('-')[0]}/${endpoint}`, {
                 params: {
                     code: wwLib.globalContext.browser.query.code,
                     oauth_token: wwLib.globalContext.browser.query.oauth_token,
