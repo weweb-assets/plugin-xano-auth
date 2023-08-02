96a0a1ea58cc541624cafb68416b9f7de98a718c
Alexis JC <alexisjc.dev@gmail.com>
2023-08-02 11:20:13 +0200

    chore: label design

--- src/components/DataSource/SettingsEdit.vue
+++ src/components/DataSource/SettingsEdit.vue
@@ -1,11 +1,11 @@
 <template>
-    <p class="mb-1 label">
+    <p class="mb-1 label-md">
         X-Data-Source Header
         <a class="xano-settings-edit__link" href="https://docs.xano.com/database/data-sources" target="_blank">
             Learn more
         </a>
     </p>
-    <p class="mb-3 label-sm">
+    <p class="mb-3 label-sm text-dark-600">
         This will only affect Dynamic collections. Static and Cached collections will always use the Production value.
     </p>
     <wwEditorInputRow
